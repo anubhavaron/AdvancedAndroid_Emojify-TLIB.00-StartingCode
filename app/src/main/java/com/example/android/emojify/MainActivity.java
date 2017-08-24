f3a92d63172ed2619aaabfa80b6bf4f3bdc5d521
anubhav1996 <anubhavaron0000051@gmail.com>
2017-08-25 02:48:43 +0530

    Getting emoji name+Added emoji finally

--- app/src/main/java/com/example/android/emojify/MainActivity.java
+++ app/src/main/java/com/example/android/emojify/MainActivity.java
@@ -180,12 +180,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Resample the saved image to fit the ImageView
         mResultsBitmap = BitmapUtils.resamplePic(this, mTempPhotoPath);
+        Emojifier e=new Emojifier();
+        mResultsBitmap=e.detectFacesAndOverlayEmoji(this,mResultsBitmap);
 
         // Set the new bitmap to the ImageView
         mImageView.setImageBitmap(mResultsBitmap);
 
-        Emojifier e=new Emojifier();
-        e.detectFaces(this,mResultsBitmap);
+
+
     }
 
 
